a594bc846fde088fedd5cd0169f4211686d6e468
Foued AMRI <amri.foued@gmail.com>
2020-10-12 14:16:51 +0200

    add custom exceptin to common module

---
+++ Wamya-backend-common/src/main/java/com/excentria_it/wamya/common/exception/UserMobileNumberValidationException.java
@@ -0,0 +1,10 @@
+package com.excentria_it.wamya.common.exception;
+
+public class UserMobileNumberValidationException extends RuntimeException {
+
+	private static final long serialVersionUID = -6897273754618266776L;
+
+	public UserMobileNumberValidationException(String message) {
+		super(message);
+	}
+}
