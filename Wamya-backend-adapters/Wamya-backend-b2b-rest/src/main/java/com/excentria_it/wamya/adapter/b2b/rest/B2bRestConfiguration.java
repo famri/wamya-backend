82e3112c646a74d40be72bce4c9091629b1fa1e9
Foued AMRI <amri.foued@gmail.com>
2021-03-13 15:06:53 +0100

    increment memory wih jvm args

--- Wamya-backend-adapters/Wamya-backend-b2b-rest/src/main/java/com/excentria_it/wamya/adapter/b2b/rest/B2bRestConfiguration.java
+++ Wamya-backend-adapters/Wamya-backend-b2b-rest/src/main/java/com/excentria_it/wamya/adapter/b2b/rest/B2bRestConfiguration.java
@@ -1,10 +1,12 @@
 package com.excentria_it.wamya.adapter.b2b.rest;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,6 +21,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class B2bRestConfiguration {
 
 	@Bean
+	@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 	public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder, ObjectMapper mapper) {
 		RestTemplate restTemplate = restTemplateBuilder.errorHandler(new RestTemplateResponseErrorHandler(mapper))
 				.requestFactory(HttpComponentsClientHttpRequestFactory.class)
