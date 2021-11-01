627e9b29b5a9f42940a70fe7b3d6100edbf46474
Foued AMRI <amri.foued@gmail.com>
2021-11-02 00:02:33 +0100

    fix cors

--- Wamya-spring-cloud/gateway/src/main/java/com/excentria_it/wamya/springcloud/gateway/SecurityConfig.java
+++ Wamya-spring-cloud/gateway/src/main/java/com/excentria_it/wamya/springcloud/gateway/SecurityConfig.java
@@ -6,16 +6,20 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.server.resource.web.server.ServerBearerTokenAuthenticationConverter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.ServerAuthenticationConverter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
 	@Bean
 	SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeExchange()
+		http.csrf().disable().cors().and().authorizeExchange()
 				.pathMatchers("/actuator/**", "/eureka/**", "/wamya-backend/login/**", "/wamya-backend/accounts/**",
 						"/oauth/**", "/wamya-backend/countries/**", "/wamya-backend/locales/**",
-						"/wamya-backend/genders/**", "/wamya-backend/content/**", "/wamya-backend/documents/**")
+						"/wamya-backend/genders/**", "/wamya-backend/content/**", "/wamya-backend/documents/**",
+						"/wamya-backend/places/**", "/wamya-backend/departments/**", "/wamya-backend/engine-types/**")
 				.permitAll().anyExchange().authenticated().and().oauth2ResourceServer()
 				.bearerTokenConverter(authenticationConverter()).jwt();
 
@@ -31,4 +35,10 @@ public class SecurityConfig {
 		return authenticationConverter;
 	}
 
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
 }
